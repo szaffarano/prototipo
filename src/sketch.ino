b679b751eaa07b987198f46dd180c7b1cdc2239b
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2013-07-09 21:04:45 -0300

    avances con DS1820, relay y RTC

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,38 +1,155 @@
 #include <Wire.h>
+#include <DallasTemperature.h>
+#include <OneWire.h>
 #include <LiquidCrystal_SR.h>
+#include <RTClib.h>
 
-//LiquidCrystal_SR lcd(8,7,TWO_WIRE);
-//LiquidCrystal_SR lcd(3,2,4);
-LiquidCrystal_SR lcd(2,1,3);
-//                   | |
-//                   | \-- Clock Pin
-//                   \---- Data/Enable Pin
+#define BUTTON_1	6
+#define BUTTON_2	7
+#define BUTTON_3	8
+#define BUTTON_4	9
 
-// Creat a set of new characters
-byte armsUp[8] = {0b00100,0b01010,0b00100,0b10101,0b01110,0b00100,0b00100,0b01010};
-byte armsDown[8] = {0b00100,0b01010,0b00100,0b00100,0b01110,0b10101,0b00100,0b01010};
+#define RTC_SDA		2
+#define RTC_SCL		3
+
+#define HEAT		3
+#define COLD		4
+
+#define TEMP_SENSOR	2
+
+#define DEBOUNCE_DELAY	50
+
+#define CLOCK		10
+#define DATA		11
+#define LATCH		12
+
+#define READ_INTERVAL	500
+
+long lastTempRead;
+
+void printTime();
+
+typedef struct {
+   int pin;
+   int state;
+   long lastDebounceTime;
+   int lastState;
+   boolean pushed;
+} button;
+
+button buttons[4];
+
+LiquidCrystal_SR lcd(DATA, CLOCK, LATCH);
+
+OneWire oneWire(TEMP_SENSOR);
+DallasTemperature sensors(&oneWire);
+
+RTC_DS1307 RTC;
 
 void setup(){
+   Serial.begin(9600);
 
-  lcd.begin(16,2);               // initialize the lcd
+   Wire.begin();
+   RTC.begin();
 
-  lcd.createChar (0, armsUp);    // load character to the LCD
-  lcd.createChar (1, armsDown);    // load character to the LCD
+   if (! RTC.isrunning()) {
+      RTC.adjust(DateTime(__DATE__, __TIME__));
+   }
 
-  lcd.home ();                   // go home
-  lcd.print("LiquidCrystal_SR");
+   buttons[0].pin = BUTTON_1;
+   buttons[1].pin = BUTTON_2;
+   buttons[2].pin = BUTTON_3;
+   buttons[3].pin = BUTTON_4;
+
+   for (int i = 0; i < 4; i++) {
+      buttons[i].state = LOW;
+      buttons[i].lastState = LOW;
+      buttons[i].lastDebounceTime = 0;
+      buttons[i].pushed = false;
+      pinMode(buttons[i].pin, INPUT);
+   }
+
+   digitalWrite(HEAT, HIGH);
+   digitalWrite(COLD, HIGH);
+
+   pinMode(HEAT, OUTPUT);
+   pinMode(COLD, OUTPUT);
+
+   lastTempRead = millis();
+
+   sensors.begin();
+
+   lcd.begin(16,2);
+
+   //lcd.home ();
+   //lcd.print("Presiona una tecla");
 }
 
 void loop(){
-  // Do a little animation
-  for(int i = 0; i <= 15; i++) showHappyGuy(i);
-  for(int i = 15; i >= 0; i--) showHappyGuy(i);
+   lcd.home ();
+   printTime();
+   lcd.setCursor (0, 1);
+   updateButtons();
+   for(int i = 0; i < 4; i++) {
+      if (buttons[i].pushed) {
+         lcd.print("X");
+      } else {
+         lcd.print("0");
+      }
+   }
+
+   digitalWrite(HEAT, buttons[0].pushed ? LOW : HIGH);
+   digitalWrite(COLD, buttons[1].pushed ? LOW : HIGH);
+
+   if (millis() - lastTempRead > READ_INTERVAL) {
+      sensors.requestTemperatures();
+      lcd.print(" ");
+      lcd.print(sensors.getTempCByIndex(0));
+      lcd.print(" C");
+      lastTempRead = millis();
+   }
+
 }
 
-void showHappyGuy(int pos){
-  lcd.setCursor ( pos, 1 ); // go to position
-  lcd.print(char(random(0,2))); // show one of the two custom characters
-  delay(150); // wait so it can be seen
-  lcd.setCursor ( pos, 1 ); // go to position again
-  lcd.print(" "); // delete character
+void updateButtons() {
+   //digitalWrite(LATCH, LOW);
+   //uint8_t reg = shiftIn(DATA, CLOCK, MSBFIRST);
+   //Serial.println(reg);
+   //reg = shiftIn(DATA, CLOCK, LSBFIRST);
+   //Serial.println(reg);
+   //digitalWrite(LATCH, HIGH);
+
+   for (int i = 0; i < 4; i++) {
+      int reading = digitalRead(buttons[i].pin);
+      //uint8_t reading = reg & i;
+
+      if (reading != buttons[i].lastState) {
+         buttons[i].lastDebounceTime = millis();
+      } 
+  
+      if ((millis() - buttons[i].lastDebounceTime) > DEBOUNCE_DELAY) {
+         if (reading != buttons[i].state) {
+            buttons[i].state = reading;
+            buttons[i].pushed = reading;
+         }
+      }
+      buttons[i].lastState = reading;
+   }
+}
+
+void printTime() {
+   DateTime now = RTC.now();
+
+   lcd.print(now.day(), DEC);
+   lcd.print('.');
+   lcd.print(now.month(), DEC);
+   lcd.print('.');
+   lcd.print(now.year()-2000, DEC);
+   lcd.print(' ');
+   lcd.print(now.hour(), DEC);
+   lcd.print(':');
+   lcd.print(now.minute(), DEC);
+   lcd.print(':');
+   lcd.print(now.second(), DEC);
+   lcd.println();
 }
